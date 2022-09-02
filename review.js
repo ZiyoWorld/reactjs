7faafb6331939bea496a2bd14a94f768439c5a8d
Mirziyod Sunatillayev <mirziyodsunatillayev1612@gmail.com>
2022-09-03 01:07:10 +0300

    change riview

--- review.js
+++ review.js
@@ -4,4 +4,36 @@
 // 1. Node js install 
 // 2. npx create-react-app and yarn create react-app
 // 3. yangi papka yaratish
-// 4.   
+// 4. react server 3000 da yuradi
+// 5. yarn
+// 6. ReadMe File
+// 7. package.json haqida 
+// 8. dependencies scripts
+// 9. git ignore
+// 10. node_modules
+// 11. src files
+// 12. Single Page Application
+// 13. index.js
+// 14. React and React DOM
+// 15. React VirtualDOM
+// 16. ReactDOM.render()
+// 17. jsx.format
+// 18. react.StrickMode
+// 19. Hi React- in the browser
+// 20. function return
+// 21. import 
+// 22. O'zim shug'ullanaman()
+
+    //  with teacher
+
+    // Birinchi dars
+
+// npx create-react-app
+// package.json, src, public
+// single page applicate
+// virtual ReactDOM
+// ReactDOM.render()
+// jsx
+// import
+
+// props, components
