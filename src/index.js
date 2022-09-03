d1e10a22b1a9eb44d933f7f1961d802588848162
Mirziyod Sunatillayev <mirziyodsunatillayev1612@gmail.com>
2022-09-03 14:08:58 +0300

    deleted files

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import fruits from './fruits';
-import Fruit from "./Fruit";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className='container'>
-            <h1 className='header'>Fruit</h1>
-    <section className='main' id='main'>
-    {
-      fruits.map( (value)=> {
-        return(
-          <Fruit data={value} />
-        )
-      })
-    }
-
-    </section>
-    
-    </div>
-    
+    <h1>Salomlar</h1>
   </React.StrictMode>
 );
 
