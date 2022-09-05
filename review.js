5274bd61051c5990dd72eeb7f917da99e2f027c8
Mirziyod Sunatillayev <mirziyodsunatillayev1612@gmail.com>
2022-09-05 06:49:50 +0300

    uchinchi_dars boshlandi

--- review.js
+++ review.js
@@ -10,26 +10,10 @@
 // props, children
 // components
 
- // O'zim o'rgangan narsalarim
-// 1. function test - ✔
-// 2.props and prop +
-// 3.git branch and with github working +
-// 4. github chiroyli yuklaymiz +
-// 5. JavaScript classes + 
-// 6. Components, React Components + 
-// 7. export and import + 
-// 8. props and component +
-// 9. export default holati haqida +
-// 10. Object name o'zgartirish + 
-// 11. component ichida 2 ta class ochish + 
-// 12. props parametr + 
-// 13. this kalit so'zi + 
-// 14. prop bilan + 
-// 15. yo'q bo'lgan qiymat undifiend +
-// 16. Props bilan object yuborish +
-// 17. ? bu bilan object ishlatish + 
-// 18. children props+
-// 19. ES6+ foydalanamiz+
-// 20. jsx format bilan ishlaymiz+
-// 21. uyga vazifa Fruits app+
 
+
+// Uchunchi dars 
+ //  --------------- with Teacher
+
+  // O'zim o'rgangan narsalarim
+// new lesson 
