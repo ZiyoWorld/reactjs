a12b1de74bf8fb200c0a17151405b70b3766eb48
Mirziyod Sunatillayev <mirziyodsunatillayev1612@gmail.com>
2022-09-06 10:29:41 +0300

    uchinchi dars davomi

--- review.js
+++ review.js
@@ -21,3 +21,22 @@
 // Uyga vazifada ustoz Cardlarni hammasini ko'paytirish bilan foydalanibdi. men esa map bilan qilib quyaqoldim.
 
 // State
+ // 1. UYga vazifa takrorlash
+ //2. Bu vazifada Teacher Cardlarni Ko'paytirib qilgan ekan. Men esa sekingina mapdan foydalandim Fruits.map() uni githubga netlifyga joyladim.
+ // 3. State.jsx yangi fayl ochamiz
+ // 4. plus-minus in JavaScript da boshqatdan qilib ko'raman
+ // 5. constructor() lifecycle- super(props) -bilan ishlayman
+ // 6. super(props) - React.Componentga parametr berib yuboramiz
+ // 7. this.state - doimo object ko'rinishida keladi.
+ // 8. render() constructor() - console.log da
+ // 9. setState() - counter
+ // 10. setState renderni qayta ishga tushuradi
+ // 11. state and setState - ekranni kuzatib turadi
+ // 12. counter yasaymiz +-
+ // 13. input onChange(event bilan)
+ // 14. select optionlar bilan
+ // 15. checkbox bilan ishlaymiz
+ // 16. uyga vazifa Table-Fixed bilan ishlaymiz.
+
+// Yangi loyiham endigi Uz Airways bo'ladi.
+
