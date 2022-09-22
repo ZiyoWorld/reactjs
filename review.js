4211e8d4b23fbb9c5077584c64916c65743fe5c0
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2022-09-22 12:47:54 +0300

    three homeworks

--- review.js
+++ review.js
@@ -1,30 +1,4 @@
-// ===============================
-// ===============================
+/// homework 3 
 
-// 5-lesson
-
-// in lesson time 
-// CRUD
-// Advanced
-
-// self-study
-
-// 1.state CRUD
-// 2.Advanced
-// table yasaymiz 
-// table delete function bilan ishlaymiz.
-
-// 5. add files bilan ishlaymmiz
-// 6. filterni to'g'rilab ishlatish
-// 7. filter zo'r chiqdida 😎😎
-// 8. edit update qilamiz
-// 9. editda xatoliklar bor
-// 10. Asosiy keylar ya'ni CRUD
-// 11. Crud create, read, update, delete
-// 12. Stateni yaxshi tugatish
-// 13. Barcha topshiriqlarni qilish
-// 14. Ajoyib vazifa tayyorlash ✔
-// 15. Table editni ham o'zgartirish
-// 16. Yangi branch ochib uyga vaizfalrni Saqlayman.
-
-=======
+// 1. I work with data
+// 2. 
--- src/App.js
+++ src/App.js
@@ -1,16 +1,137 @@
 import React, {Component} from "react";
-
-
+import {data} from './data';
 
 
 class App extends Component {
   
   state = {  
+     modalBtn: false,
+     data: data,
+     id: '',
+     name: '',
+     type: '',
+     address: '',
+     phone: '',
+     email: '',
 
   }
   render() { 
+      const {name, address, phone, type,  data, email, modalBtn} = this.state;
+      const onChange = (e)=>{
+          this.setState({
+            [e.target.name]: e.target.value,
+          });
+      };
+
+      const onOpenBtn = ()=>{
+        this.setState({
+          modalBtn: true,
+        })
+      }
+      const onCloseBtn = ()=>{
+        this.setState({
+          modalBtn: false
+        })
+      };
+
+      const onNewCard = ()=>{
+        let user = {
+          id: Date.now(),
+          name: name,
+          address: address,
+          type: type,
+          phone: phone,
+          email: email,
+        }
+        console.log(user);
+        
+          this.setState({
+            data: [...data, user],
+            id: "",
+            name: "",
+            type: '',
+            address: '',
+            phone: '',
+            email: '',
+          })
+        
+        
+      };
+
+      const onCloseModal = ()=>{
+        this.setState({
+          id: '',
+          name: '',
+          type: '',
+          address: '',
+          phone: '',
+          email: '',
+        })
+      }
+   
+    
     return ( 
-      <h1> Hello world</h1>
+        <div className="container">
+            <h1>Sizning do'konlariz</h1>
+            <div className="shoppings">
+              {
+                data.map( (value)=>{
+                  return(
+                    <div className="card" key={value.id}>
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMt_i-1uZdBIPkdVegkSj9HAJbp7wx2usc3VLhXUFcwQ&s" alt="" />
+                      <h2>TexnoMart</h2>
+                      <p>Owner</p>
+                      <button onClick={onOpenBtn}>Open</button>
+                      {
+                       modalBtn ? (<div className="modaldata">
+                         <h5>Company name: {value.name}</h5>
+                        <h5>Address: {value.address}</h5>
+                        <h5>Type: {value.type}</h5>
+                        <h5>Phone:{value.phone}</h5>
+                        <h5>Email:{value.email}</h5>
+                        <button onClick={onCloseBtn}>Close</button>
+                       </div>) : ("")
+                      }
+                    </div>
+                  )
+                })
+              }
+              <div className="add">
+                <a href="#modal">+</a>
+                <p>Add new shop</p>
+              </div>
+            </div>
+            
+
+           <h1>Sotvchi sifatida ro'yxatdan o'tish </h1>
+             <div className="modalbox">
+
+                <div  className="modal" id="modal">
+                  <label htmlFor="name">Kompaniya nomi</label>
+                  <input type="text"  id="name" name="name" value={name} onChange={onChange} />
+                  <label htmlFor="address">Kompaniya manzili</label>
+                  <input type="text"  id="address" name="address" value={address} onChange={onChange} />
+                  <label htmlFor="type">Faoliyat turi</label>
+                  <select onChange={onChange} name="type" id="type">
+                    <option value="">Tanlang:</option>
+                    <option value="savdo">Online Savdo</option>
+                    <option value="mobile" >Mobile</option>
+                    <option value="auto" >Auto</option>
+                  </select>
+                  <label htmlFor="phone">Telefon raqam</label>
+                  <input type="text"  name="phone" id="phone" value={phone} onChange={onChange} />
+                  <label htmlFor="email">Email manzilni yuboring</label>
+                  <input type="text"  name="email" id="email" value={email} onChange={onChange} />
+                  <div className="buttons">
+                  <button type="submit" onClick={ onNewCard }>Confirm</button>
+                  <button onClick={onCloseModal}>Close</button>
+                </div>
+              </div>
+
+
+
+            </div>
+        </div>
      );
   }
 }
