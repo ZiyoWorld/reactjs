cb5f48511f84187f13d3b0d330f559cd75a8e811
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2022-09-27 23:04:09 +0300

    styled-components 2

--- review.js
+++ review.js
@@ -1,24 +1,8 @@
-// React Course 6 - lesson
+// ========= ReactJs 7 ======
+// --inheritance
+// --keyframes => animation
+// --prototypes
+// --project
+// --theme
 
-// ===== Styled Components =======
-
-// 1. styled -components css o'rnida ishlatiladi.
-// 2. takrorlash CRUD
-// 3. css larni JavaScript ichida ishlatib ketamiz
-// 4. npm i styled-components & yarn add styled-components;
-// 5. import styled from 'styled-components'
-// 6. ikki xil usulda ishlatamiz styled("h1") & styled.h1
-// 7. styleddda ishlaymiz
-// 8. box bilan ishlaymiz
-// 9. get_size function bilan ishlaymiz
-// 10 css components ishlatamiz common css
-
-
-// ==== lesson 
-// styled components 
-// css
-// props 
-// function call
-
-
-
+// 
