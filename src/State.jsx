f5d0febbe757bd7409aeb85fd822ec8c42a01b23
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2022-10-10 09:13:05 +0300

    new react file

---
+++ src/State.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Container } from './styleState';
+
+class State extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <Container>
+                
+            </Container>
+         );
+    }
+}
+ 
+export default State;
