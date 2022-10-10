83518bb6f955f871fcb9d1c5a88569f328f462f0
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2022-10-10 09:40:44 +0300

    finishid

--- src/State.jsx
+++ src/State.jsx
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
-import { Container } from './styleState';
+import React, {Component} from "react";
+import {Box, Container, Desc, Title} from './styleState';
+
 
 class State extends Component {
+    
     state = {  }
     render() { 
-        return ( 
-            <Container>
-                
-            </Container>
-         );
+        return ( <Container>
+            <Box bg="black" type="large">Large</Box>
+            <Box bg="yellow" type="medium">Medium</Box>
+            <Box bg="red"  type="small">Small</Box>
+            <Title>Hey Title</Title>
+            <Desc left>Description</Desc>
+            <Desc>Description</Desc>
+        </Container> );
     }
 }
  
--- src/styleState.jsx
+++ src/styleState.jsx
@@ -1,5 +1,49 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
-export const Container = styled.div`
-    background-color: coral;
+export const Container = styled('div')`
+display: flex;
+background-color: coral;
+color: black;
+padding: 20px;
+`;
+
+const common = css`
+    font-weight: 400;
+    font-family: bold;
+    padding: 10px;
+    border: 1px solid black;
+    height: fit-content;
 `
+
+export const Title = styled.h1`
+    color: blue;
+    ${common}
+`;
+
+export const Desc = styled.h1`
+    ${common};
+    color: ${({left})=> left ? "red" : "green"};
+`;
+
+const getSize = (props)=>{
+    switch(props.type){
+        case 'large' : return '200px';
+        case 'medium' : return '150px';
+        case 'small' : return '100px';
+        default : return "150px"
+    }
+}
+
+export const Box = styled.div`
+background-color: ${(props)=> props.bg};
+display: flex;
+justify-content: center;
+align-items: center;
+/* width: ${(props) => props.type === 'large' ? '200px': '150px'};
+height: ${(props) => props.type === 'large' ? '200px': '150px'}; */
+width: ${getSize};
+height: ${getSize};
+border: 2px solid black;
+margin: 0 10px;
+font-size: 24px;
+`;
