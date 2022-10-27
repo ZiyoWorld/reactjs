bfe9bc7362557eb7ba8709a6667892cf1defdbfb
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2022-10-27 23:05:11 +0300

    use reducer

--- src/Hook.jsx
+++ src/Hook.jsx
@@ -1,33 +1,40 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useReducer} from "react";
 
-const Hooks = (props)=>{
-  const [count, setCount] = useState(props.count);
+const Hooks = ()=>{
+  const [count, setCount] = useState(0);
   const [name, setName] = useState('Mirziyod');
 
-  // useEffect
-  useEffect( ()=> {
-    // console.log('case 1');
-  });
+  const reducer = (state, action)=> {
+    switch(action.type){
+      case 'plus' : return state + 1;
+      case 'minus' : return state - 1;
+      case 'byamount' : return state + action.payload;
+      default: return state; 
+    }
+  }
 
-  useEffect( ()=>{
-    console.log("case 2");
-  }, []);
-
-  useEffect( ()=>{
-    console.log("case 3")
-    setCount(props.count);
-  }, [props.count]);
-
-  useEffect( ()=>{
-    console.log("case 4");
-  }, [name, count]);
-
+  const [counter, dispatch] = useReducer( reducer, 0);
+  const [change, setChange] = useState(1);
+  function onHandler(e){
+    setChange(e.target.value);
+  }
   return(
     <div style={{flex: 1}}>
       <h1>Hooks Component</h1>
       <h1>Count: {count}</h1>
-      <h1>Name: {name}</h1>
+      <h1>Counter: {counter}</h1>
+      <select name="" id=""
+      onChange={onHandler} >
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <button onClick={()=>dispatch({type: "byamount", payload: Number(change),})}>{change}</button>
 
+      <button onClick={()=>dispatch({type: "minus"})}>-</button>
+      <button onClick={()=>dispatch({type: "plus"})}>+</button>
       <input type="text" value={name} 
       onChange={({target})=> setName(target.value)}
       />
