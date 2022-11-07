bc10fc780962aa04a28410e391ef8cac54dea925
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2022-11-07 10:43:26 +0300

    axios vs fetch

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,19 +1,34 @@
+import axios from "axios";
 import React from "react";
-import Fetch from "../components/Fetch";
-import Login from "../components/Login";
-import Add from "../components/Add";
-import Update from "../components/Update";
-
+// import Fetch from "../components/Fetch";
+// import Login from "../components/Login";
+// import Add from "../components/Add";
+// import Update from "../components/Update";
+import Axios from '../components/Axios';
+import Fetchapi from '../components/Fetchapi';
 import { Container } from "./style";
 
 function App(){
 
+  axios.interceptors.request.use( (request)=>
+  {
+    console.log(request, "request");
+    request.headers.Authorization = "Bearer token";
+    return request
+  } )
+  axios.interceptors.response.use( (responce)=> {
+    console.log(responce, "responce");
+
+  })
+
     return(
         <Container>
-          <Login />
+          <Axios />
+          <Fetchapi />
+          {/* <Login />
           <Fetch />
           <Add />
-          <Update />
+          <Update /> */}
         </Container>
     )
 }
