cc1026f450d5686563ea0ea1494059fe562b1b41
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2022-11-09 08:55:07 +0300

    now only props and componets

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,35 +1,17 @@
-import axios from "axios";
-import React from "react";
-// import Fetch from "../components/Fetch";
-// import Login from "../components/Login";
-// import Add from "../components/Add";
-// import Update from "../components/Update";
-import Axios from '../components/Axios';
-import Fetchapi from '../components/Fetchapi';
-import { Container } from "./style";
-
-function App(){
-
-  axios.interceptors.request.use( (request)=>
-  {
-    console.log(request, "request");
-    request.headers.Authorization = "Bearer token";
-    return request
-  } )
-  axios.interceptors.response.use( (responce)=> {
-    console.log(responce, "responce");
-
-  })
-
-    return(
-        <Container>
-          <Axios />
-          <Fetchapi />
-          {/* <Login />
-          <Fetch />
-          <Add />
-          <Update /> */}
-        </Container>
-    )
+import React from 'react'
+import Student from '../components/Student';
+const Root = ()=> {
+  return (
+    <div>
+        <Student name="Mirziyod" age="19" country="Uzbekistan" />
+        <Student name="Mirziyod" age="19" country="Uzbekistan" />
+        <Student name="Mirziyod" age="19" country="Uzbekistan" />
+        <Student name="Mirziyod" age="19" country="Uzbekistan" />
+        <Student name="Mirziyod" age="19" country="Uzbekistan" >
+          <h1>Uzr Go'zal from Bahrom Nazarov</h1>
+        </Student>
+    </div>
+  )
 }
-export default App;
+
+export default Root;
--- test.js
+++
@@ -1,4 +0,0 @@
-const obj1 = {title: "Mirziyod"};
-const obj2 = {title: "Mirziyod"};
-
-console.log(obj1 === obj2);
