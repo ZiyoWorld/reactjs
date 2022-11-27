aaf765f6e04ebc36548658e62582b866716fc7d0
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2022-11-27 22:37:47 +0300

    shular

---
+++ review.js
@@ -0,0 +1,184 @@
+// Birinchi dars
+
+// npx create-react-app
+// package.json, src, public
+// single page applicate
+// virtual ReactDOM
+// ReactDOM.render()
+// jsx
+// import
+
+// =======================
+// =======================
+
+// Ikkinchi dars
+// props, components
+// export import
+// props, children
+// components
+
+// =======================
+// =======================
+
+// uchinchi dars
+// state
+// constructor
+// super
+// onChange, onClick
+// input, checkbox, select
+
+// =======================
+// =======================
+
+// To'rtinchi dars
+// function call
+// multi state change
+// student list => add delete filter
+
+// =======================
+// =======================
+// beshinchi dars
+// styled
+// { css }
+// props
+// function call
+
+// =======================
+// =======================
+// inheritance
+// keyframse -> animation
+// prototypes
+// project
+// theme
+
+// =======================
+// =======================
+// YouTube Clone
+// Lifting state up
+// Search movies
+// delete moveies
+
+// =======================
+// =======================
+// if()
+// ternary operator (?)
+// navbar, attr, container, Components
+
+// vazifa
+// 1 => 2ta components => signin, signup
+// 2 => UI
+// 3 => functional buttons
+
+// =======================
+// =======================
+// ReactJS => 7 Lifecycles
+// bind => 3 ways
+
+// =======================
+// =======================
+// Class vs Hooks components
+// return vs without return hooks components
+// this in class and hooks
+// useState
+// primitive vs non primitive
+
+// =======================
+// =======================
+// useEffect
+
+// =======================
+// =======================
+// useReducer
+
+// =======================
+// =======================
+// state management?
+// prop drilling?
+// lifting state up?
+
+// =======================
+// =======================
+// useContext
+// createContext
+// put to the store
+// useContext
+
+// =======================
+// =======================
+// useContext advanced
+// useContext
+// file management
+// how to make general context
+// useReducer with useContext
+
+// =======================
+// =======================
+// local Storage
+// LocalStorage.setItem(name, value);
+// localStorage.getItem(name);
+// localStorage.removeItem(name);
+// localStorage.clear();
+
+
+
+// react - optimazation , memo => child Component trigger re-render
+// memo - asosiy yordam beruvchi buyruq 2 ta qoidasi doim child parentda re-render bo'lish 
+// trigger, re-render
+// memo and useMemo()
+// ikki object bir biriga teng bo'lmaydi.
+// memo and use Memo memo
+// useCallback() vs useMemo 
+
+// =======================
+// =======================
+
+// useRef
+// Controlled and UnControlled  
+
+// =======================
+// =======================
+
+// restFull API
+// fetch 
+// endpoint, url, link, api
+// CRUD
+// GET, PUT, POST, DELETE
+// swagger
+// token
+// auth = > authorization
+// 200 => 201=> 401=> 404=> 415=> 500
+
+// =======================
+// =======================
+
+// axios
+// yarn add axios
+// axios and fetch 
+// Vanilla js and 3rd part library
+// manually vs automatic
+
+// =======================
+// =======================
+// React Router Dom 5
+// Browser Router
+// Nawlink, Link
+// Router
+// UseHistory, UseLocation => window.location 
+// Switch
+// usePrams
+// privateRoute
+
+
+// =======================
+// =======================
+ // React Router Dom 6
+ //redirect => navigate
+ // useHistory => useNavigate
+ // NavLink activeStyle => style={({isActive})}
+// Router, Routes 
+// component => element
+//  Outlet 
+// useParams
+// privateRoute
+
+
---
+++ src/root/index.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import { Container } from './style';
+
+const Root = ()=> {
+ 
+  return (
+   <Container>
+    <h1>Class</h1>
+   </Container>
+  )
+}
+
+export default Root;
+
+
